57b589532c5c26be6ac27f731f9d537f08113d5a
Derek <star1216@gmail.com>
2017-12-12 09:42:33 +0800

    update

--- include.php
+++ include.php
@@ -1,23 +1,26 @@
+<?php define('root_dir', '/old_v/sh-epoxy/'); ?>
+
+
 <link rel="icon" href="favicon.png" type="image/png">
-<link href="css/bootstrap.css" rel="stylesheet" type="text/css">
-<link href="css/style.css" rel="stylesheet" type="text/css">
-<link href="css/linecons.css" rel="stylesheet" type="text/css">
-<link href="css/font-awesome.css" rel="stylesheet" type="text/css">
-<link href="css/responsive.css" rel="stylesheet" type="text/css">
-<link href="css/animate.css" rel="stylesheet" type="text/css">
+<link href="<?php echo root_dir ?>css/bootstrap.css" rel="stylesheet" type="text/css">
+<link href="<?php echo root_dir ?>css/style.css" rel="stylesheet" type="text/css">
+<link href="<?php echo root_dir ?>css/linecons.css" rel="stylesheet" type="text/css">
+<link href="<?php echo root_dir ?>css/font-awesome.css" rel="stylesheet" type="text/css">
+<link href="<?php echo root_dir ?>css/responsive.css" rel="stylesheet" type="text/css">
+<link href="<?php echo root_dir ?>css/animate.css" rel="stylesheet" type="text/css">
 
 <link href='http://fonts.googleapis.com/css?family=Lato:400,900,700,700italic,400italic,300italic,300,100italic,100,900italic' rel='stylesheet' type='text/css'>
 <link href='http://fonts.googleapis.com/css?family=Dosis:400,500,700,800,600,300,200' rel='stylesheet' type='text/css'>
 
 <!--[if IE]><style type="text/css">.pie {behavior:url(PIE.htc);}</style><![endif]-->
 
-<script type="text/javascript" src="js/jquery.1.8.3.min.js"></script>
-<script type="text/javascript" src="js/bootstrap.js"></script>
-<script type="text/javascript" src="js/jquery-scrolltofixed.js"></script>
-<script type="text/javascript" src="js/jquery.easing.1.3.js"></script>
-<script type="text/javascript" src="js/jquery.isotope.js"></script>
-<script type="text/javascript" src="js/wow.js"></script>
-<script type="text/javascript" src="js/classie.js"></script>
+<script type="text/javascript" src="<?php echo root_dir ?>js/jquery.1.8.3.min.js"></script>
+<script type="text/javascript" src="<?php echo root_dir ?>js/bootstrap.js"></script>
+<script type="text/javascript" src="<?php echo root_dir ?>js/jquery-scrolltofixed.js"></script>
+<script type="text/javascript" src="<?php echo root_dir ?>js/jquery.easing.1.3.js"></script>
+<script type="text/javascript" src="<?php echo root_dir ?>js/jquery.isotope.js"></script>
+<script type="text/javascript" src="<?php echo root_dir ?>js/wow.js"></script>
+<script type="text/javascript" src="<?php echo root_dir ?>js/classie.js"></script>
 
 <!--[if lt IE 9]>
     <script src="js/respond-1.1.0.min.js"></script>
---
+++ 工程解析/sidebar.php
@@ -0,0 +1,8 @@
+<h1 class="my-5">Epoxy工法說明介紹</h1>
+<div class="list-group">
+  <a href="#" class="list-group-item active">Epoxy - 彩繪晶E-101 耐重壓地坪</a>
+  <a href="#" class="list-group-item">Epoxy - 彩繪晶E-103停車場止滑地坪</a>
+  <a href="#" class="list-group-item">Epoxy - 彩繪晶E-105流展地坪</a>
+  <a href="#" class="list-group-item">Epoxy - 彩繪晶E-107流展地坪</a>
+  <a href="#" class="list-group-item">Epoxy - 彩繪晶E-109導電地坪</a>
+</div>
